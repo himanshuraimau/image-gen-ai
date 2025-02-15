59f3983e35714f752461f03d63916832348e4ef7
Himanshu Rai <himanshuraimau9@gmail.com>
2025-02-16 01:05:17 +0530

    feat(db): initialize db package with Prisma schema and basic setup

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import express from 'express';
+import { TrainModel, GenerateImage, GenerateImagesFromPack } from '@repo/common/types';
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
